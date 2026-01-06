a86c5bae5dc17c33d30f7ce281771c4684ad516c
Emilia-Z <keirinzeng@gmail.com>
2026-01-06 12:13:20 +1100

    LC-ransom-note [Runtime: 9 ms; Memory: 46.4 MB]

---
+++ hash-table/ransom-note.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public bool CanConstruct(string ransomNote, string magazine) {
+        Dictionary<char, int> map = new Dictionary<char, int>();
+
+        foreach( char c in magazine ){
+            if(map.ContainsKey(c)){
+                map[c]++;
+            }else{
+                map[c] = 1;
+            }
+        }
+
+        
+        foreach( char c in ransomNote){
+            if(!map.ContainsKey(c)){
+                return false;
+            }
+            map[c]--;
+            if(map[c]<0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
