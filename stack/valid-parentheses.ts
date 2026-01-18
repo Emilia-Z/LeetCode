b7633fff4f37755a13f86b9e7555267f241b96f0
Emilia-Z <keirinzeng@gmail.com>
2026-01-18 20:28:06 +1100

    LC-valid-parentheses [Runtime: N/A; Memory: N/A]

---
+++ stack/valid-parentheses.ts
@@ -0,0 +1,20 @@
+function isValid(s: string): boolean {
+    const stack: string[] = [];
+    const map: Record<string, string> = {
+        ')': '(',
+        '}':'{',
+        ']':'[',
+    };
+
+    for(const ch of s){
+       if(ch ==='(' || ch ==='{' || ch === '['){
+            stack.push(ch);
+       }else{
+        if(stack.length==0) return false;
+        const top = stack.pop();
+        if(top !== map[ch]) return false;
+       }
+       
+    }
+    return true;
+};
