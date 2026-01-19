30895f9aff445d6058bcb6ee1e0d017779894b59
Emilia-Z <keirinzeng@gmail.com>
2026-01-19 23:58:22 +1100

    LC-climbing-stairs [Runtime: 0 ms; Memory: 53.8 MB]

--- math/climbing-stairs.ts
+++ math/climbing-stairs.ts
@@ -6,7 +6,7 @@ function climbStairs(n: number): number {
     dp[1] = 1;
     dp[2] = 2;
     for(let i = 3; i<= n; i++){
-        dp[n] = dp[n-1] + dp[n-2];
+        dp[i] = dp[i-1] + dp[i-2];
     }
     return dp[n];
 
