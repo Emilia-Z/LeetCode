6d50d6b60dc59185a2c96066abdc48d262cee446
Emilia-Z <keirinzeng@gmail.com>
2026-03-15 19:02:11 +1100

    LC-search-insert-position [Runtime: 0 ms; Memory: 55.5 MB]

--- array/search-insert-position.ts
+++ array/search-insert-position.ts
@@ -2,18 +2,19 @@ function searchInsert(nums: number[], target: number): number {
     let left = 0;
     let right = nums.length - 1;
     
-    while(left < right){
+    while(left <= right){
         let middle = Math.floor((left + right)/2);
         if(nums[middle]>target){
-            right = middle;
+            right = middle - 1;
         }
         else if(nums[middle]<target){
             left = middle + 1;
         }
-        else return middle;
+        else{
+        return middle;}
     }
-    if (nums[left]>=target){return left;}
-    else{ return right+1;}
+    return left;
+    
 
     
 };
