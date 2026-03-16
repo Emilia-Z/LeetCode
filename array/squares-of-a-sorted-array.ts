d77f1c277cb66c9aacb92e935f7cf87ec723545a
Emilia-Z <keirinzeng@gmail.com>
2026-03-16 16:15:01 +1100

    LC-squares-of-a-sorted-array [Runtime: 2 ms; Memory: 63.1 MB]

--- array/squares-of-a-sorted-array.ts
+++ array/squares-of-a-sorted-array.ts
@@ -5,11 +5,11 @@ function sortedSquares(nums: number[]): number[] {
     }
     let a = 0;
     let b = nums.length - 1;
-    let result = [];
+    let result = new Array(nums.length);
     let point=nums.length-1;
     while(a<=b){
         
-        if(nums[a]>=nums[b]){
+        if(nums[a]>nums[b]){
             result[point]=nums[a];
             a++;
             
