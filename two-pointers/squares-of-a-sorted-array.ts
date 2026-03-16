a201414e708dd69013c0808a155a66ce6ab51fb8
Emilia-Z <keirinzeng@gmail.com>
2026-03-16 16:12:24 +1100

    LC-squares-of-a-sorted-array [Runtime: 18 ms; Memory: 64.2 MB]

---
+++ two-pointers/squares-of-a-sorted-array.ts
@@ -0,0 +1,25 @@
+function sortedSquares(nums: number[]): number[] {
+    
+    for(let i = 0; i<nums.length; i++){
+        nums[i] = nums[i]*nums[i];
+    }
+    let a = 0;
+    let b = nums.length - 1;
+    let result = [];
+    let point=nums.length-1;
+    while(a<=b){
+        
+        if(nums[a]>=nums[b]){
+            result[point]=nums[a];
+            a++;
+            
+        }
+        else{
+            result[point]=nums[b];
+            b--;
+        }
+        point--;
+    }
+    return result;
+    
+};
